549b61608e0bafaf2c090c909b707921228e07d7
Djdan-byte <daniel1gon2ram@gmail.com>
2026-09-02 23:04:55 -0600

    pokemones

--- index.js
+++ index.js
@@ -1,22 +1,41 @@
-/*const express=require('express');
-const app=express();
+const express = require('express');
+const app = express();
+const { pokemon } = require('./pokedex.json');
 
- Verbos HTTP
-GET
-POST
-PATCH
-PUT
-DELETE
-
-
-app.get('/', (req,res,next)=>{
+app.get("/", (req, res, next) => {
     res.status(200);
-    res.send("Bienvenido");
+    res.send("Bienvenido al Pokedex");
 });
 
-app.listen(3000, () =>{
-    console.log("Srever is running...");
+app.get('/pokemon/all', (req, res, next) => {
+    res.status(200);
+    res.send(pokemon);
 });
-*/
 
-console.log("Hola mundo");
+app.get('/pokemon/:id', (req, res, next) => {
+    const id = req.params.id - 1;
+    if (id >= 0 && id <= 150) {
+        res.status(200);
+        res.send(pokemon[id]);
+    }
+    else {
+        res.status(404);
+        res.send("Pokémon no encontrado");
+    }
+});
+
+app.get('/pokemon/:name', (req, res, next) => {
+    const name = req.params.name;
+    for (i = 0; i < pokemon.length; i++) {
+        if (pokemon[i].name == name) {
+            res.status(200);
+            return res.send(pokemon[i]);
+        }
+    }
+    res.status(404);
+    res.send("Pokémon no encontrado");
+});
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Server is running...");
+});
